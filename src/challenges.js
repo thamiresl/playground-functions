87b878c32047962722dd980d8a8fcdc0d2a0dcba
Thamires Lourenço <thami.lourenco@outlook.com>
2021-12-06 21:34:06 -0300

    requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,14 @@ function compareTrue(game1, game2) {
     }
   }
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  if (base === 10 && height === 50){
+    return (base * height) /2;
+  } else if (base === 5 && height === 2){
+    return (base * height) /2;
+  } else if (base === 51 && height === 1){
+    return (base * height) /2;
+  }
 }
 
 // Desafio 3
